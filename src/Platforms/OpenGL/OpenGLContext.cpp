87089cf234d2711b40c23d05623d9662bc2190b7
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-11-07 16:29:25 +0100

    Add OpenGL Vendor and Version logging.

--- src/Platforms/OpenGL/OpenGLContext.cpp
+++ src/Platforms/OpenGL/OpenGLContext.cpp
@@ -16,6 +16,11 @@ void OpenGLContext::Init() {
 
   int gladInitialized = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
   FR_CORE_ASSERT(gladInitialized, "Failed to intialize GLAD!")
+
+  FR_CORE_INFO("OpenGL Renderer:");
+  FR_CORE_INFO("  Vendor:   {0}", glGetString(GL_VENDOR));
+  FR_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+  FR_CORE_INFO("  Version:  {0}", glGetString(GL_VERSION));
 }
 
 void OpenGLContext::SwapBuffers() { glfwSwapBuffers(m_WindowHandle); }
