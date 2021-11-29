1f90628e1d7e429f62db861d43e4559046a344df
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-11-29 20:22:08 +0100

    Add more uniform functions.

--- src/Platforms/OpenGL/OpenGLShader.hpp
+++ src/Platforms/OpenGL/OpenGLShader.hpp
@@ -14,7 +14,12 @@ public:
   virtual void Bind() const override;
   virtual void Unbind() const override;
 
+  void UploadUniformInt(const std::string& name, const int& value);
+  void UploadUniformFloat(const std::string& name, const float& value);
+  void UploadUniformFloat2(const std::string& name, const glm::vec2& value);
+  void UploadUniformFloat3(const std::string& name, const glm::vec3& value);
   void UploadUniformFloat4(const std::string& name, const glm::vec4& value);
+  void UploadUniformMat3(const std::string& name, const glm::mat3& value);
   void UploadUniformMat4(const std::string& name, const glm::mat4& value);
 
 private:
